10d51028b35d2fa30c280d0f2c88a9903c77e7bd
Ezra Fermanis & James Sangalli <pair+ezrafermanis.james-sangalli@devbootcamp.com>
2016-04-01 12:06:27 +1300

    add description data

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var request = require("superagent")
 // var dotenv = require("dotenv")
 var fs = require("fs")
 
-request.get("https://www.googleapis.com/books/v1/volumes?q=harry+potter&callback=handleResponse",function(req,res){
-  console.log(res.text[0]["description"],req);
-  fs.writeFile("./data.json",res.text,"utf8")
+request.get("https://www.googleapis.com/books/v1/volumes?q=harry+potter",function(req,res){
+  console.log(JSON.parse(res.text).items[0].volumeInfo.description)
+  //console.log(res.body)
+  fs.writeFile("./data.json",JSON.parse(res.text).items[0].volumeInfo.description,"utf8")
 })
