00811a320e4802a22db0d39266980b83e6a83f3c
Ezra Fermanis & James Sangalli <pair+ezrafermanis.james-sangalli@devbootcamp.com>
2016-04-01 15:46:53 +1300

    add test

---
+++ test.js
@@ -0,0 +1,10 @@
+var test = require('tape')
+var index = require("./index.js")
+
+test("if data exists from server", function(t){
+  t.equal(data, typeof 'string', "should return turning");
+  t.end();
+});
+
+
+
